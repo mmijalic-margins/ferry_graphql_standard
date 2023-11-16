a6cf8653d974290a9902b27ac9efd4c930c8e996
mmijalic-margins <marin.mijalic@valeremargins.com>
2023-11-16 13:14:32 +0100

    dynamic sending of data

--- lib/films/data/source/films_source.dart
+++ lib/films/data/source/films_source.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: inference_failure_on_function_invocation
 
 import 'package:injectable/injectable.dart';
-import 'package:loggy/loggy.dart';
 import 'package:standard/graphql/graphql_client.dart';
 import 'package:standard/graphql/queries/models/film.data.gql.dart';
 import 'package:standard/graphql/queries/models/film.req.gql.dart';
@@ -12,20 +11,12 @@ class FilmsSource {
       : _graphQLClass = graphQLClass;
 // source that generates roadblocks game
   Future<GAllFilmsData?> fetchFilms() async {
-    GAllFilmsData? filmsData;
     final reviewsReq = GAllFilmsReq(
       (b) => b,
     );
-    final returnable = await _graphQLClass.client
-        .request(reviewsReq)
-        .timeout(const Duration(seconds: 7))
-        .first;
-    if (returnable.hasErrors == true) {
-      throw Exception('error');
-    } else {
-      filmsData = returnable.data;
-    }
-    return filmsData;
+    final returnable = await _graphQLClass.requestQuery(request: reviewsReq);
+
+    return returnable as GAllFilmsData;
   }
 
   final GraphQLClass _graphQLClass;
--- lib/graphql/graphql_client.dart
+++ lib/graphql/graphql_client.dart
@@ -1,8 +1,9 @@
-// ignore_for_file: join_return_with_assignment
+// ignore_for_file: join_return_with_assignment, strict_raw_type
 
 import 'package:ferry/ferry.dart';
 import 'package:gql_http_link/gql_http_link.dart';
 import 'package:injectable/injectable.dart';
+import 'package:loggy/loggy.dart';
 
 @singleton
 class GraphQLClass {
@@ -23,6 +24,22 @@ class GraphQLClass {
     );
 
     return client;
+  }
+
+  Future<dynamic> requestQuery({
+    required OperationRequest request,
+  }) async {
+    dynamic returnable;
+    logInfo('REQUEST SENT:');
+    final dataSource =
+        await client.request(request).timeout(const Duration(seconds: 7)).first;
+
+    if (dataSource.hasErrors == true) {
+      throw Exception('error');
+    } else {
+      returnable = dataSource.data;
+    }
+    return returnable;
   }
 
   late Client client;
