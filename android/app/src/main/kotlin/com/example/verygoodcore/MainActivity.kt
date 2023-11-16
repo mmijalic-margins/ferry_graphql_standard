4092f0ec6ac81aea8ab8a98c6dc679abf6d86426
mmijalic-margins <marin.mijalic@valeremargins.com>
2023-11-16 10:21:15 +0100

    init commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.standard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:standard/app/app.dart';
+import 'package:standard/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
